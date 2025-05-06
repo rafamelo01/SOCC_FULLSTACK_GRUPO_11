dc3a4a3a1cef834f060802235b1ce0c9edd359aa
Rafael Melo <rafaelmelo4000@gmail.com>
2025-05-06 08:46:58 -0300

    first commit

---
+++ src/test/java/_1/socc_manterusuario/SoccManterusuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package _1.socc_manterusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccManterusuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
